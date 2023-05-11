d60ebbe9e77d4ae88ae3146f5e55a6b29435d8a6
nlaureta <nate.laureta@gmail.com>
2023-05-10 22:55:34 -0700

    added placeholder menu

--- apps/birds/controllers.py
+++ apps/birds/controllers.py
@@ -4,10 +4,19 @@ from .models import get_user_email
 from .settings import APP_FOLDER
 import os, json
 
-@action('index')
+@action('/birds', method=['GET'])
+@action.uses('index.html', auth)
+def redirect_to_main_menu():
+    redirect(URL('mainMenu'))
+
+@action('mainMenu')
+@action.uses('mainMenu.html', auth)
+def mainMenu():
+    return dict()
+
+@action('/index')
 @action.uses('index.html', auth)
 def index():
-    filename = os.path.join(APP_FOLDER, "data", "table.json")
-    f = open(filename)
-    data = json.load(f)
-    return dict(data=data)
+    return dict()
+
+
