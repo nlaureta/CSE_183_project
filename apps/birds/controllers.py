df84e6a67afd75941b9f26d75aef8f3d7c099992
nlaureta <nate.laureta@gmail.com>
2023-05-22 23:05:55 -0700

    changes to homepage and index
    wordGuess added as well but not functional

--- apps/birds/controllers.py
+++ apps/birds/controllers.py
@@ -5,29 +5,37 @@ from .settings import APP_FOLDER
 import os, json
 import random
 
-@action('/birds', method=['GET'])
-@action.uses('index.html', auth)
+@action('/birds/index', method=['GET'])
+@action.uses('index.html', auth.user)
 def redirect_to_main_menu():
     redirect(URL('mainMenu'))
 
 @action('mainMenu')
-@action.uses('mainMenu.html', auth)
+@action.uses('mainMenu.html', auth.user)
 def mainMenu():
     return dict()
 
 @action('/index')
-@action.uses('index.html', auth)
+@action.uses('index.html', db,  auth.user)
 def index():
-    return dict()
-
-@action("words")
-@action.uses(db, 'wordTest.html')
-def words():
     randid = random.randrange(1, 10)
     rows = db(db.words.id == randid).select()
     return dict(rows=rows)
 
+
+@action("wordGuess", method=["GET", "POST"])
+@action.uses('wordGuess.html',db,  auth.user )
+def words():
+    if request.method == "POST":
+        guessed_word = request.forms.get("guess").lower() 
+        correct_word = "apple"  
+        
+        if guessed_word == correct_word:
+            redirect(URL('/index'))
+    
+    return dict()
+
 @action("homepage")
-@action.uses(db, 'homepage.html')
+@action.uses('homepage.html', auth.user )
 def homepage():
     return dict()
