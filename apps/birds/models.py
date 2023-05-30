bd2de7e5d21e8ef100cea19a86bff1304b9f245e
Mi-No <m0697@naver.com>
2023-05-30 12:34:57 -0700

    save image as url in db

--- apps/birds/models.py
+++ apps/birds/models.py
@@ -3,22 +3,33 @@ This file defines the database models
 """
 
 import datetime
+import random
+from py4web.utils.populate import FIRST_NAMES, LAST_NAMES, IUP
 from .common import db, Field, auth
 from pydal.validators import *
-
 
 def get_user_email():
     return auth.current_user.get('email') if auth.current_user else None
 
+def get_username():
+    return auth.current_user.get('username') if auth.current_user else None
+
 def get_time():
     return datetime.datetime.utcnow()
-
 
 ### Define your table below
 #
 # db.define_table('thing', Field('name'))
 #
 ## always commit your models to avoid problems later
+
+db.define_table(
+    'draw',
+    Field('word_id'),
+    Field('user_id'),
+    Field('url'),
+    auth.signature
+)
 
 db.define_table(
     'words',
